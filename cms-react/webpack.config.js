31e6fb9a11f6af85956fddea2422bf92c3937ab7
sjeksani <sjeksani@users.noreply.github.com>
2017-06-15 21:03:37 -0700

    Add files via upload

---
+++ cms-react/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        app: './main.js',
+        //vendor: ['react', 'react-dom'],
+    },
+
+    output: {
+        path: path.resolve(__dirname, './folder'),
+        filename: '[name].bundle.js',
+        publicPath: '/assets',
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: { presets: ['es2015', 'react'] },
+                }],
+            }, {
+                test: /\.jsx$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: { presets: ['es2015', 'react'] },
+                }],
+            },
+
+            // Loaders for other file types can go here
+
+        ]
+    },
+    /*plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'commons.js',
+            minChunks: 2,
+        }),
+    ],*/
+    devServer: {
+        contentBase: path.resolve(__dirname, './src'), // New
+    },
+};
